12c7fe9b3ff521370a84badc959038f9c501495f
sergey_p <wild-tiger1990@mail.ru>
2017-03-07 16:56:58 +0000

    обновлен 7 марта

---
+++ migrations/m170307_164457_film.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m170307_164457_film extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170307_164457_film cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}

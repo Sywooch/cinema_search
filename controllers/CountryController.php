eca90fa5d91ee91ad32ca588c4ba206c42c58f60
proproshka <pro@autosputnik.ru>
2017-03-12 12:21:41 +0300

    popravil kosak

--- controllers/CountryController.php
+++ controllers/CountryController.php
@@ -17,9 +17,14 @@ class CountryController extends \yii\web\Controller
     public function actionView($id)
     {
         $country = Country::find()->where(['id'=>$id])->one();
+//        echo $id.'<br>';
+//        echo '<pre>';
+//        var_dump($country);
+//        echo '</pre>';
 
         return $this->render('view',[
-            'singleCountry' => (is_null($country))?'fgf':$country
+//            'singleCountry' => ((is_null($country))?'fgf':$country)
+            'singleCountry' => $country
         ]);
     }
 
--- views/country/view.php
+++ views/country/view.php
@@ -1,5 +1,11 @@
 <?php
-
-echo $singleCountry;
+/*
+    echo '<pre>';
+    var_dump($singleCountry);
+    echo '</pre>';
+    foreach ($singleCountry as $key => $value )
+        echo $key.'-----'.$value;
+*/
+    echo '<br>'.'<br>'.'<br>'.'<br>'.'<br>'.'<br>'.$singleCountry['name'];
 
 ?>

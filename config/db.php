129f792ccd95fd8727e754a12e88f3459b8df0ac
proproshka <pro@autosputnik.ru>
2017-03-13 13:16:49 +0300

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost:3306;dbname=db_cinema_search',
-    'username' => 'procinema',
-    'password' => 'pr0l0g',
-    'charset' => 'utf8',
-];

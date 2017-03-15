86c4097a348f8dc11e62f8aa4f9b4b4be65c367b
sergey_p <wild-tiger1990@mail.ru>
2017-03-15 15:52:59 +0000

    Удалил db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost:3306;dbname=pro',
-    'username' => 'pro',
-    'password' => '257358',
-    'charset' => 'utf8',
-];

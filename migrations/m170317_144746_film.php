3d1f46e2472cf49aea2aba543419a7753eae0f3f
sergey_p <wild-tiger1990@mail.ru>
2017-03-20 09:47:39 +0000

    Миграции для всех таблиц в этом файле

--- migrations/m170317_144746_film.php
+++ migrations/m170317_144746_film.php
@@ -6,13 +6,19 @@ class m170317_144746_film extends Migration
 {
     public function up()
     {
+        // создаю таблицу
         $this->createTable('{{%film}}', [
             'id' => Schema::TYPE_PK,
             'name' => Schema::TYPE_STRING . ' NOT NULL',
             'year' => Schema::TYPE_INTEGER,
-            'email' => Schema::TYPE_STRING . ' NOT NULL',
             'note' => Schema::TYPE_STRING,
             'country_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+        ]);
+
+        $this->createTable('{{%film_genre}}', [
+            'id' => Schema::TYPE_PK,
+            'film_id' => Schema::TYPE_INTEGER,
+            'genre_id' => Schema::TYPE_INTEGER,
         ]);
 
         $this->createTable('{{%country}}', [
@@ -20,14 +26,165 @@ class m170317_144746_film extends Migration
             'name' => 'VARCHAR(45)',
         ]);
 
-         // creates index for column `author_id`
+        $this->createTable('{{%genre}}', [
+            'id' => Schema::TYPE_PK,
+            'name' => 'VARCHAR(45)',
+        ]);
+
+        $this->createTable('{{%comments}}', [
+            'id' => Schema::TYPE_PK,
+            'note' => 'VARCHAR(500)',
+            'date_comment' => Schema::TYPE_DATETIME,
+            'film_id' => Schema::TYPE_INTEGER,
+            'user_id' => Schema::TYPE_INTEGER,
+        ]);
+
+        $this->createTable('{{%user}}', [
+            'id' => Schema::TYPE_PK,
+            'name' => 'VARCHAR(45)',
+            'email' => Schema::TYPE_STRING,
+            'password' => 'VARCHAR(45)',
+            'create_time' => Schema::TYPE_DATETIME,
+        ]);
+
+        $this->createTable('{{%film_persona}}', [
+            'id' => Schema::TYPE_PK,
+            'film_id' => Schema::TYPE_INTEGER,
+            'persona_id' => Schema::TYPE_INTEGER,
+            'type_persona_id' => Schema::TYPE_INTEGER,
+        ]);
+
+        $this->createTable('{{%persona}}', [
+            'id' => Schema::TYPE_PK,
+            'name' => 'VARCHAR(45)',
+            'surname' => 'VARCHAR(45)',
+            'date_dr' => Schema::TYPE_DATETIME,
+            'note' => Schema::TYPE_STRING,
+            'photo' => 'VARCHAR(255)',
+            'books' => 'VARCHAR(100)',
+            'count_film' => Schema::TYPE_INTEGER,
+            'tvShow' => 'VARCHAR(255)',
+        ]);
+
+        $this->createTable('{{%type_persona}}', [
+            'id' => Schema::TYPE_PK,
+            'type' => 'VARCHAR(45)',
+        ]);
+
+
+        // создается индекс от которого идет связь
+        $this->createIndex(
+            'idx-film_persona-type_persona',
+            '{{%film_persona}}',
+            'type_persona_id'
+        );
+
+        // создается внешний ключ
+        $this->addForeignKey(
+            'fk-film_persona-type_persona',
+            '{{%film_persona}}',
+            'type_persona_id',
+            '{{%type_persona}}',
+            'id',
+            'CASCADE'
+        );
+
+        $this->createIndex(
+            'idx-film_persona-persona_id',
+            '{{%film_persona}}',
+            'persona_id'
+        );
+
+        $this->addForeignKey(
+            'fk-film_persona-persona_id',
+            '{{%film_persona}}',
+            'persona_id',
+            '{{%persona}}',
+            'id',
+            'CASCADE'
+        );
+        
+        $this->createIndex(
+            'idx-film_persona-film_id',
+            '{{%film_persona}}',
+            'film_id'
+        );
+
+        $this->addForeignKey(
+            'fk-film_persona-film_id',
+            '{{%film_persona}}',
+            'film_id',
+            '{{%film}}',
+            'id',
+            'CASCADE'
+        );
+
+        $this->createIndex(
+            'idx-comments-user_id',
+            '{{%comments}}',
+            'user_id'
+        );
+
+        $this->addForeignKey(
+            'fk-comments-user_id',
+            '{{%comments}}',
+            'user_id',
+            '{{%user}}',
+            'id',
+            'CASCADE'
+        );
+
+        $this->createIndex(
+            'idx-comments-film_id',
+            '{{%comments}}',
+            'film_id'
+        );
+
+        $this->addForeignKey(
+            'fk-comments-film_id',
+            '{{%comments}}',
+            'film_id',
+            '{{%film}}',
+            'id',
+            'CASCADE'
+        );
+
+        $this->createIndex(
+            'idx-film_genre-genre_id',
+            '{{%film_genre}}',
+            'genre_id'
+        );
+
+        $this->addForeignKey(
+            'fk-film_genre-genre_id',
+            '{{%film_genre}}',
+            'genre_id',
+            '{{%genre}}',
+            'id',
+            'CASCADE'
+        );
+
+         $this->createIndex(
+            'idx-film_genre-film_id',
+            '{{%film_genre}}',
+            'film_id'
+        );
+
+          $this->addForeignKey(
+            'fk-film_genre-film_id',
+            '{{%film_genre}}',
+            'film_id',
+            '{{%film}}',
+            'id',
+            'CASCADE'
+        );
+
         $this->createIndex(
             'idx-film-country_id',
             '{{%film}}',
             'country_id'
         );
 
-        // add foreign key for table `country`
         $this->addForeignKey(
             'fk-film-country_id',
             '{{%film}}',

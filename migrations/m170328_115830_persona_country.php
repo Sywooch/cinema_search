912314ae276743f8377acee1ee937ee53fa9f0a6
sergey_p <wild-tiger1990@mail.ru>
2017-03-28 12:09:55 +0000

    Миграция для персон и стран

--- migrations/m170328_115830_persona_country.php
+++ migrations/m170328_115830_persona_country.php
@@ -3,27 +3,25 @@
 use yii\db\Migration;
 use yii\db\Schema;
 
-class m170327_174029_persona_country extends Migration
+class m170328_115830_persona_country extends Migration
 {
     public function up()
     {
-        $this->addColumn(
+         $this->addColumn(
             "{{%persona}}",
             "country_id",
             "int(11)");
-
-        $this->createIndex(
+         $this->createIndex(
             'country_id',
             '{{%persona}}',
-            'country_id',
-            true);
+            'country_id');
 
         $this->addForeignKey("lg_fk", "{{%persona}}", "country_id", "{{%country}}", "id", 'RESTRICT');
     }
 
     public function down()
     {
-        echo "m170327_174029_persona_country cannot be reverted.\n";
+        echo "m170328_115830_persona_country cannot be reverted.\n";
 
         return false;
     }
